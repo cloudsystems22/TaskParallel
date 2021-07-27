16a8fb926d07960aec67971642c2884eb8958c1d
cloudsystems22 <cloudsystems22@gmail.com>
2021-07-27 19:46:48 -0300

    first commit

---
+++ TaskParallel/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Parallel.Invoke(
+                () => ExecutThreadZero(), 
+                () => ExecutThreadUm(), 
+                () => ExecutThreadDot()
+                );
+            
+            stopwatch.Stop();
+            Console.WriteLine("Tarefa finalizada! {0}s", stopwatch.ElapsedMilliseconds / 1000.0);
+        }
+
+        private static void ExecutThreadDot()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                TaskSleep();
+                Console.Write(".");
+            }
+        }
+
+        private static void ExecutThreadUm()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                TaskSleep();
+                Console.Write("1");
+            }
+        }
+
+        private static void ExecutThreadZero()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                TaskSleep();
+                Console.Write("0");
+            }
+        }
+
+        private static void TaskSleep()
+        {
+            Thread.Sleep(10);
+        }
+    }
+}
